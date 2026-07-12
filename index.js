72891544f892efe9d6fe68bd6e70dfa623ea8f60
NuruddinJewel <m.76jewel@gmail.com>
2026-07-12 23:52:10 +0600

    Database Configured

---
+++ index.js
@@ -0,0 +1,50 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const Stripe = require('stripe');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Crowdfunding backend running')
+})
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
