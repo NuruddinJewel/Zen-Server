aadcd3972f6174c1efa986cedf9bd92a5e74038f
NuruddinJewel <m.76jewel@gmail.com>
2026-07-14 22:07:51 +0600

    Updated Deployment

--- index.js
+++ index.js
@@ -115,10 +115,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:3000",
-//     credentials: true,
-// }));
+app.use(cors({
+    origin: "https://zen-rise-two.vercel.app",
+    credentials: true,
+}));
 app.use(cors());
 app.use(express.json());
 
