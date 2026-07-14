89e7d7e5cced86edd34fe8f3b684abd901bd2684
NuruddinJewel <m.76jewel@gmail.com>
2026-07-14 14:46:30 +0600

    Routes Updated

--- routes/report.routes.js
+++ routes/report.routes.js
@@ -1,14 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const { getReports, updateReportStatus } = require("../controllers/report.controller");
-// const { requireAuth, requireRole } = require("../middleware/auth.middleware");
-
-// router.get("/", requireAuth, requireRole("admin"), getReports);
-// router.patch("/:id/status", requireAuth, requireRole("admin"), updateReportStatus);
-
-// module.exports = router;
-//2
-
 const express = require("express");
 const router = express.Router();
 const { getReports, updateReportStatus, createReport } = require("../controllers/report.controller");
